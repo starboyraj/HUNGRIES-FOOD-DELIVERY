63b0e55d855a041a992bcdbc7b05af334785c468
raj_singh <realestate9835@gmail.com>
2026-01-10 09:11:41 +0530

    add assets and Navbar file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <img src={assets.logo} alt='' className='logo' />
+        <ul className='navbar-menu'>
+            <li>home</li>
+            <li>menu</li>
+            <li>mobile-app</li>
+            <li>contact us</li>
+        </ul>
+        <div className="navbar-right">
+           <img src={assets.search_icon} alt='' />
+           <div className='navbar-search-icon'>
+             <img src={assets.basket_icon} alt='' />
+             <div className='dot'></div>
+           </div>
+           <button className='navbar-button'>Sign up</button>
+        </div>
+    </div>
   )
 }
 
