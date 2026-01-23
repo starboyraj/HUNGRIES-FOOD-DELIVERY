5168895f42a2ec8cf4990a81c0a3567bda393040
Raj Singh <starboyraj88828@gmail.com>
2026-01-24 01:10:29 +0530

    Create Hero Section and lists

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import './Header'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and elevate your dining experience meal at a time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
   )
 }
 
